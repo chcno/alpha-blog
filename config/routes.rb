bd9d7ff640cefcf05a6730b90821b687dbbe8838
Charlie Cano <charliecno04@gmail.com>
2022-05-27 10:52:38 -0500

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
